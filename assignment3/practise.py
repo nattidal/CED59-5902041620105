50acf94d617b428e5934653481015fb4507a18f2
nattidal <52274114+nattidal@users.noreply.github.com>
2019-07-17 22:08:26 +0700

    Create practise.py

---
+++ assignment3/practise.py
@@ -0,0 +1,9 @@
+mylist = [40,30,30]
+print(sum(mylist))
+
+my_tuple = (5,20,7,13)
+print(my_tuple)
+
+set1 = {5,2,61,77,80,9,20,12,66}
+set2 = {4,5,17,9,83,12,65,32,77}
+print (set1.intersection(set2))
