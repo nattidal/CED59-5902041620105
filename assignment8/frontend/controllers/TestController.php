89b771878558676a664a837bf969292143df8dac
Computer Education <>
2019-09-05 16:37:14 +0700

    add file

---
+++ assignment8/frontend/controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class TestController extends \yii\web\Controller
+{
+    public function actionIndex() {
+        echo 'data Test';
+        return $this->render('index', [ 'xdata' => $data]);
+    }
+
+    public function actionTest() {
+        echo 'Nattida';
+    }
+}
